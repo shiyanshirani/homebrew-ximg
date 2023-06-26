a5fd30a7df8eff22593268882e4b8982f6bbec2d
shiyanshirani <shiyan99s@gmail.com>
2023-06-26 15:26:03 +0530

    build: brew formula

--- ximg.rb
+++ ximg.rb
@@ -1,16 +1,13 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ximg < Formula
   desc ""
   homepage ""
-  url "https://github.com/shiyanshirani/ximg/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "620c10804826e8d5671444bee353677c9a0bf11974700bc8a589ffdf3179493a"
+  url "https://github.com/shiyanshirani/ximg/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "b471030d2097246f68ba1e359b47c4ae52142a0d7c1385e66fbad88fb3d1b081"
   license ""
 
-  # depends_on "cmake" => :build
 
   def install
-    bin.install "main"
+      bin.install "ximg"
   end
+
 end
